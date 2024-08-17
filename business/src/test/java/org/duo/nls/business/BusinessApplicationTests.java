75a931d50023aad0f7a2fc0783e705ebedbc0218
Administrator <gentleduo@163.com>
2024-08-17 12:59:59 +0800

    初始化项目

---
+++ business/src/test/java/org/duo/nls/business/BusinessApplicationTests.java
@@ -0,0 +1,13 @@
+package org.duo.nls.business;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
